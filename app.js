32f11d4573b5ebdff9047663a007fecb3f454c99
ismael strey <ismaelstrey@hotmail.com>
2019-07-17 16:28:47 -0300

    configuração docker mongo DB

--- app.js
+++ app.js
@@ -12,36 +12,43 @@ app.set('views', './views')
 app.set('view engine', 'jade')
 
 app.route('/')
-    .get(function(req, resp){
+    .get(function (req, resp) {
         console.log('Enviado curso get')
-        listarCursos(resp)
-        
+        var data
+        data = listarCursos(resp)
+        console.log(data)
+
     })
-    .post(function(req, resp){
+    .post(function (req, resp) {
         console.log('Enviado curso via post')
-        var curso = { nome: req.body.nome, categoria: req.body.categoria};
-        inserirCurso(curso, function(){
+        var curso = { nome: req.body.nome, categoria: req.body.categoria };
+
+        console.log(curso)
+        inserirCurso(curso, function () {
             listarCursos(resp)
         })
 
     })
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('Servidor rodando na porta 3000')
 })
-function listarCursos(resp){
-    MongoClient.connect('mongodb://172.30.30.98:27017', { useNewUrlParser: true }, function(err, database){
+function listarCursos(resp) {
+    MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, function (err, database) {
         const myDB = database.db('express');
-        myDB.collection('cursos').find().sort({nome: 1}).toArray(function(err, result){
+        myDB.collection('cursos').find().sort({ nome: 1 }).toArray(function (err, result) {
             console.log(333, result)
-            resp.render('index', {data: result});
+            resp.render('index', { data: result });
+            if (err) {
+                console.log("Erro ao tentar realizar a gavação dos dados", err)
+            }
         })
     })
- }
-function inserirCurso(obj, callback){
-    MongoClient.connect('mongodb://172.30.30.98:27017', { useNewUrlParser: true }, function(err, database){
-        const myDB = database.db('express');    
-        myDB.collection('cursos').insertOne(obj, function(err, result){
+}
+function inserirCurso(obj, callback) {
+    MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, function (err, database) {
+        const myDB = database.db('express');
+        myDB.collection('cursos').insertOne(obj, function (err, result) {
             callback()
         })
     })
